daad266fbe7fa76cc887de1ad9b566b27b4b4147
Mike Jerome <mjerome@financialforce.com>
2017-10-09 12:24:05 +0100

    Output celebrities in age order on default route

--- models/person.js
+++ models/person.js
@@ -1,14 +1,23 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  var Person = sequelize.define('Person', {
-    name: DataTypes.STRING,
-    birth_date: DataTypes.DATEONLY
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
+  var Person = sequelize.define(
+    "Person",
+    {
+      name: DataTypes.STRING,
+      birth_date: DataTypes.DATEONLY
+    },
+    {
+      classMethods: {
+        associate: function(models) {
+          // associations can be defined here
+        }
       }
     }
-  });
+  );
+
+  Person.prototype.age = function() {
+    return this.getDataValue("age");
+  };
+
   return Person;
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var models = require("../models");
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  models.sequelize
+    .query(
+      `SELECT
+          name,
+          birth_date,
+          EXTRACT(YEAR FROM age(birth_date)) AS age
+      FROM
+          "People"
+      ORDER BY
+          birth_date DESC`,
+      {
+        model: models.Person
+      }
+    )
+    .then(function(people) {
+      res.render("index", { title: "Celebrities, ordered by age", people: people });
+    });
 });
 
 module.exports = router;
